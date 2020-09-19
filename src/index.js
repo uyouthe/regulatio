1e85d691633342f570f25b2f0621de09bf2c6ece
uyouthe <hello@miloslav.website>
2020-09-19 23:16:43 +0300

    unsubs added

--- src/index.js
+++ src/index.js
@@ -10,18 +10,24 @@ export default (element, filterFunction) => {
         'drop',
     ]
 
+    const regulate = function () {
+        if (filterFunction(this.value)) {
+            this.oldValue = this.value
+            this.oldSelectionStart = this.selectionStart
+            this.oldSelectionEnd = this.selectionEnd
+        } else if (this.hasOwnProperty('oldValue')) {
+            this.value = this.oldValue
+            this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd)
+        } else {
+            this.value = ''
+        }
+    }
+
     eventTypes.forEach(eventType => {
-        element.addEventListener(eventType, function () {
-            if (filterFunction(this.value)) {
-                this.oldValue = this.value
-                this.oldSelectionStart = this.selectionStart
-                this.oldSelectionEnd = this.selectionEnd
-            } else if (this.hasOwnProperty('oldValue')) {
-                this.value = this.oldValue
-                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd)
-            } else {
-                this.value = ''
-            }
-        })
+        element.addEventListener(eventType, regulate)
+    })
+
+    return () => eventTypes.forEach(eventType => {
+        element.removeEventListener(eventType, regulate)
     })
 }
