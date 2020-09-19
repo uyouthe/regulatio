e614fb364ddab912bbf5f255ea0c0452cce3410e
uyouthe <hello@miloslav.website>
2020-09-19 23:17:09 +0300

    unsubs added

--- dist/regulatio.js
+++ dist/regulatio.js
@@ -13,20 +13,26 @@ var regulatio = (function () {
             'drop',
         ];
 
+        const regulate = function () {
+            if (filterFunction(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty('oldValue')) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                this.value = '';
+            }
+        };
+
         eventTypes.forEach(eventType => {
-            element.addEventListener(eventType, function () {
-                if (filterFunction(this.value)) {
-                    this.oldValue = this.value;
-                    this.oldSelectionStart = this.selectionStart;
-                    this.oldSelectionEnd = this.selectionEnd;
-                } else if (this.hasOwnProperty('oldValue')) {
-                    this.value = this.oldValue;
-                    this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-                } else {
-                    this.value = '';
-                }
-            });
+            element.addEventListener(eventType, regulate);
         });
+
+        return () => eventTypes.forEach(eventType => {
+            element.removeEventListener(eventType, regulate);
+        })
     };
 
     return index;
